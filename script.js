14fd1e37067d15ca8a2da32fc79825d417cb8d0b
Aiden Kelly <aidenquinkelly@gmail.com>
2026-02-01 15:16:07 -0800

    working on survey

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 function nextPage() {
     window.location.href = "yes.html";
 }
+function surveyPage() {
+    window.location.href = "When.html";
+}
+
+function saveTime(choice) {
+  localStorage.setItem("time", choice);
+  window.location.href = "event.html";
+}
+
+let selectedEvents = [];
+
+function toggleEvent(button, eventName) {
+  if (selectedEvents.includes(eventName)) {
+    selectedEvents = selectedEvents.filter(e => e !== eventName);
+    button.style.backgroundColor = "#ED5757";
+    button.style.color = "white";
+  } else {
+    selectedEvents.push(eventName);
+    button.style.backgroundColor = "#FAF9F6";
+    button.style.color = "#ED5757";
+  }
+}
+
+function saveEvents() {
+  localStorage.setItem("events", JSON.stringify(selectedEvents));
+  window.location.href = "location.html";
+}
+
+function saveLocation(choice) {
+  localStorage.setItem("location", choice);
+  window.location.href = "drink.html";
+}
+
 
 function moveButton() {
     var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth);
